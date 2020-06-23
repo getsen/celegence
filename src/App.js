11dafba0a3bd5ff9bbc6396e02da8a7791603e7c
getsen <45854297+getsen@users.noreply.github.com>
2020-06-23 23:08:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import Questions from "./Components/Questions";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Questions />
+    </div>
+  );
+}
---
+++ src/Components/Questions.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import QuestionList from "../data/questionsList";
+
+const Questions = () => {
+  const [question, setQuestion] = useState({});
+  const [selectedOption, setSelectedOption] = useState("");
+  const [index, setIndex] = useState(1);
+  const [isPrevBtnDisabled, setPrevBtnDisabled] = useState(true);
+  const [isNextBtnDisabled, setNextBtnDisabled] = useState(true);
+
+  useEffect(() => {
+    setSelectedOption("");
+    setPrevBtnDisabled(index <= 1);
+    setNextBtnDisabled(index === QuestionList.length);
+    const currentQuestion = QuestionList.filter(
+      question => question.id === index
+    );
+    setQuestion(currentQuestion[0]);
+  }, [index]);
+
+  const onPrevBtnClick = () => {
+    setIndex(index => index - 1);
+  };
+
+  const onNextBtnClick = () => {
+    setIndex(index => index + 1);
+  };
+
+  const onSubmitAnswer = () => {
+    if (!selectedOption) {
+      alert("Please select a Option!");
+    } else {
+      showAnswer();
+    }
+  };
+
+  const showAnswer = () => {
+    alert("The correct Answer is " + question.answer);
+  };
+
+  return (
+    <div>
+      <form>
+        <div>
+          <h2>
+            {question.id}. {question.question}
+          </h2>
+        </div>
+        <div className="options">
+          {question.options &&
+            question.options.map(opt => (
+              <div key={opt}>
+                <label>
+                  <input
+                    type="radio"
+                    value={opt}
+                    checked={selectedOption === opt}
+                    onChange={e => setSelectedOption(e.target.value)}
+                  />
+                  {opt}
+                </label>
+              </div>
+            ))}
+        </div>
+        <div className="btnGroup">
+          <span>
+            <button
+              type="button"
+              onClick={onPrevBtnClick}
+              disabled={isPrevBtnDisabled}
+            >
+              Previous
+            </button>
+          </span>
+          <span>
+            <button type="button" onClick={onSubmitAnswer}>
+              Submit Answer
+            </button>
+          </span>
+          <span>
+            <button
+              type="button"
+              onClick={onNextBtnClick}
+              disabled={isNextBtnDisabled}
+            >
+              Next
+            </button>
+          </span>
+          <span>
+            <button type="button" onClick={showAnswer}>
+              Show Answer
+            </button>
+          </span>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Questions;
